00a59e6e09946d2620ce19288c220511a546a9a1
NRGuillen <ruben.guillen.dosa@gmail.com>
2025-09-19 14:29:01 +0200

    PSP

---
+++ PSP_ProgramacionDeServiciosYProcesos/src/LanzarComandoCMD/PrincipalProceso1.java
@@ -0,0 +1,12 @@
+package LanzarComandoCMD;
+
+public class PrincipalProceso1 {
+
+	public static void main(String[] args) {
+				
+		GeneradorProceso1 lanzador = new GeneradorProceso1();
+		String ruta = "ipconfig";
+		lanzador.ejecutar(ruta);
+	}
+	
+}
