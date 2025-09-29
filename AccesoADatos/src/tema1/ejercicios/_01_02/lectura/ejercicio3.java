d2a7d9e0d3eab27e1e6141ed6cf2f79a54844c6b
NRGuillen <ruben.guillen.dosa@gmail.com>
2025-09-29 21:30:01 +0200

    Acceso de Datos

--- AccesoADatos/src/tema1/ejercicios/_01_02/lectura/ejercicio3.java
+++ AccesoADatos/src/tema1/ejercicios/_01_02/lectura/ejercicio3.java
@@ -1,10 +1,14 @@
 package tema1.ejercicios._01_02.lectura;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class ejercicio3 {
 
 	public static void main(String[] args) {
+
+		ArrayList<String> separarComillas = new ArrayList<>();
+		ArrayList<String> separarComas = new ArrayList<>();
 
 		File archivo = new File("Restaurants.csv");
 
@@ -21,11 +25,72 @@ public class ejercicio3 {
 				FileReader lector = new FileReader(archivo);
 				BufferedReader buffer = new BufferedReader(lector);
 
-				String linea = buffer.readLine().replaceAll(" ", "");
-				while (linea != null) {
-					System.out.println("Campo: " + linea);
-				
-					linea = buffer.readLine();
+				String expRegComa = ",";
+
+				String linea;
+				boolean separar = false;
+				while ((linea = buffer.readLine()) != null) {
+
+					if (linea.contains("\"")) {
+						separar = true;
+					} else {
+						separar = false;
+					}
+
+					if (!separar) {
+						String[] comas = linea.split(",");
+
+						for (int i = 0; i < comas.length; i++) {
+
+							System.out.print(" Campo: " + comas[i]);
+							separarComas.add(comas[i]);
+						}
+						System.out.println();
+
+					}
+
+					if (separar) {
+
+						int contador = 0;
+						String[] comillas = linea.split(",");
+						boolean comillasBolean = false;
+						String nuevaLinea = "";
+						for (int i = 0; i < comillas.length; i++) {
+
+							if (comillas[i].contains("\"")) {
+								contador++;
+								comillasBolean = true;
+							} else {
+								comillasBolean = false;
+
+							}
+
+							if (!comillasBolean && contador == 1 || contador == 0) {
+								comillasBolean = false;
+								comillas = linea.split(",");
+								System.out.print(/* " contador" + contador + */" Campo: " + comillas[i]);
+							}
+
+							if (contador == 1 && comillasBolean) {
+								comillas = linea.split("\"");
+								System.out.print(" Campo: \"" + comillas[i] + "\""/* " contador" + contador + */);
+								contador++;
+							}
+
+							if (contador == 2 && !comillasBolean) {
+								comillas = linea.split(",");
+								comillas[2] = "";
+
+								System.out.print(comillas[i]);
+								if (i < comillas.length - 1) { // hasta el penultimo
+									System.out.print(" ,Campo: ");
+								}
+							}
+
+						}
+						System.out.println();
+					}
+
 				}
 
 			} catch (IOException e) {
