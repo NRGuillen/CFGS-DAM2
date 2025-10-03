7bef67bd0c79851a3c7caaf289fa493af87b806e
NRGuillen <ruben.guillen.dosa@gmail.com>
2025-10-03 14:27:44 +0200

    Apuntes multimedia kotlin + java

---
+++ ProgramacionMultimedia/CodeKotlin/app/src/main/java/Persona.kt
@@ -0,0 +1,14 @@
+import android.app.Person
+
+class Persona(nombre:String, primerApellido:String, edad: Int) {
+
+    var nombre = nombre
+    var primerApellido = primerApellido
+    var edad = edad
+
+    fun main(){
+        val persona1 = Persona("Ruben", "Guillen", 20)
+        println(persona1.nombre + " " +persona1.primerApellido+ " " +persona1.edad)
+    }
+
+}
---
+++ ProgramacionMultimedia/CodeKotlin/app/src/main/java/hola.kt
@@ -0,0 +1,21 @@
+fun main() {
+
+    val numPlanetasSisemaSolar:Int = 8
+    var planeta = "tierra"
+
+    println("El numero de planeras del siste solar es " +numPlanetasSisemaSolar)
+    println("El planeta donde habitamos los humanos es "+planeta)
+    planeta = "marte"
+    println("El planeta de los marcianos es " +planeta)
+
+    //Byte
+    //Short
+    //Int
+    //Long
+    //Float
+    //Double
+    //Boolean => true o false
+    //Char => entre comillas siempre
+    //...
+
+}
