ea82d56347bb816afee68b22455a955bc973a0d6
NRGuillen <ruben.guillen.dosa@gmail.com>
2025-10-06 14:27:29 +0200

    Multimedia

--- ProgramacionMultimedia/CodeKotlin/app/src/main/java/Persona.kt
+++ ProgramacionMultimedia/CodeKotlin/app/src/main/java/Persona.kt
@@ -5,10 +5,12 @@ class Persona(nombre:String, primerApellido:String, edad: Int) {
     var nombre = nombre
     var primerApellido = primerApellido
     var edad = edad
+}
 
-    fun main(){
+
+
+fun main(args: Array<String>) {
         val persona1 = Persona("Ruben", "Guillen", 20)
         println(persona1.nombre + " " +persona1.primerApellido+ " " +persona1.edad)
     }
 
-}
--- ProgramacionMultimedia/CodeKotlin/app/src/main/java/hola.kt
+++ ProgramacionMultimedia/CodeKotlin/app/src/main/java/hola.kt
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput
+
 fun main() {
 
     val numPlanetasSisemaSolar:Int = 8
@@ -18,4 +20,48 @@ fun main() {
     //Char => entre comillas siempre
     //...
 
+    var numerito : Int = 4
+
+    while(numerito <= 25){
+        println(numerito)
+        numerito++
+    }
+
+    for (i in 0..10){ //sin poner < o <= solo 0..10
+        println(i)
+    }
+
+    for (i in 10 downTo  20){
+        println(i)
+    }
+
+    for (i in 1..20 step 2){ //De 2 en 2
+        println(i)
+    }
+
+    println("Introduce tu peso")
+    val peso = readln().toInt()
+
+    println("Introduce tu altura")
+    val altura = readln().toInt()
+
+    println("Introduce tu altura")
+    val dia = readln().toInt()
+
+    when{
+        //Switch equivalente al de java, aqui no exise el switch
+        peso > 90 && altura < 190 -> println("Apuntate al gym")
+        peso < 60 && altura < 165 -> println("Come un poco")
+        peso < 90 && altura < 195 -> println("Estas en forma")
+        else -> println("Eres promedio")
+
+    }
+
+    when(dia) {
+
+        1 -> println("lunes")
+        2 -> println("martes")
+        3 -> println("miercoles")
+        else -> println("Ni lunes, ni martes, ni miercoles")
+    }
 }
