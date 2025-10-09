9517f529ffd334beb94bb77198f7fcc9d8e6a787
NRGuillen <ruben.guillen.dosa@gmail.com>
2025-10-09 14:28:39 +0200

    Hilos Estaticos Sync

---
+++ PSP_ProgramacionDeServiciosYProcesos/src/HilosEstaticosSync/mainEstatico.java
@@ -0,0 +1,30 @@
+package HilosEstaticosSync;
+
+public class mainEstatico {
+
+	public static int EJECUCIONES = 100000000;
+	public static int HILOS = 10;
+
+	public static void main(String[] args) {
+
+		Thread[] hilos = new Thread[HILOS];
+
+		for (int i = 0; i < HILOS; i++) {
+			Thread th = new Thread(new Contadores(i, EJECUCIONES / HILOS));
+			th.start();
+			hilos[i] = th;
+
+		}
+
+		for (Thread h : hilos) {
+			try {
+				h.join(); // Bloquea el hilo principal hasta que h termine, es decir el main
+			} catch (InterruptedException e) {
+				System.out.println("Interrupcion del hilo");
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
