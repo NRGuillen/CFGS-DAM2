23e3a7b342db8aa01c90dc3eb2f3039c655d0f26
NRGuillen <ruben.guillen.dosa@gmail.com>
2025-10-15 09:27:16 +0200

    Apuntes proyecto Acceso a datos

---
+++ AccesoADatos/src/ApuntesPracticaTocha/mainClase.java
@@ -0,0 +1,48 @@
+package ApuntesPracticaTocha;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class mainClase {
+
+	public static void main(String[] args) {
+
+		try {
+
+			File ficheroXML = new File("persona.xml");
+			DocumentBuilderFactory DBF = DocumentBuilderFactory.newInstance();
+			DocumentBuilder docB = DBF.newDocumentBuilder();
+			Document doc = docB.parse(ficheroXML);
+			doc.getDocumentElement().normalize();
+			NodeList lista = doc.getElementsByTagName("persona");
+			int cantidad = lista.getLength();
+
+			for (int i = 0; i < cantidad; i++) {
+
+				Node nodo = lista.item(i);
+				if (nodo.getNodeType() == Node.ELEMENT_NODE) {
+					Element persona = (Element) nodo;
+					String nombre = persona.getElementsByTagName("nombre").item(0).getTextContent();
+					String edad = persona.getElementsByTagName("edad").item(0).getTextContent();
+					String ciudad = persona.getElementsByTagName("ciudad").item(0).getTextContent();
+					System.out.println("La persona es " + nombre + " | Edad: " + edad + " | Ciudad: " + ciudad);
+				}
+
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+
+	}
+
+}
