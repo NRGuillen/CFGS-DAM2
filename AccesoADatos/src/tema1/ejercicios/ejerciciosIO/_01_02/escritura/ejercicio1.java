490a7ad3fee517d8dd4dbf0a8f3ebc1e265e9b0e
NRGuillen <ruben.guillen.dosa@gmail.com>
2025-10-20 10:11:48 +0200

    Apuntes acceso a datos NIO

---
+++ AccesoADatos/src/tema1/ejercicios/ejerciciosIO/_01_02/escritura/ejercicio1.java
@@ -0,0 +1,58 @@
+package tema1.ejercicios.ejerciciosIO._01_02.escritura;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ejercicio1 {
+
+	public static void main(String[] args) {
+
+		ArrayList<String> primos = new ArrayList<>();
+		for (int i = 2; i < 500; i++) {
+			boolean esPrimo = true;
+
+			for (int j = 2; j < i; j++) {
+				if (i % j == 0) {
+
+					System.out.println("No primo -> " + i + " / " + j + " =  " + (i % j));
+					esPrimo = false;
+				}
+			}
+
+			if (esPrimo) {
+				System.out.println("Primo -> " + i);
+
+				Integer primo = i;
+				primos.add(primo.toString());
+			}
+		}
+
+		File archivoPrimos = new File("primos.txt");
+
+		try {
+			if (!archivoPrimos.exists()) {
+				archivoPrimos.createNewFile();
+
+			}
+
+			BufferedWriter bufferEscritura = new BufferedWriter(new FileWriter(archivoPrimos)); // escribe en el fichero
+
+			for (int i = 0; i < primos.size(); i++) {
+				bufferEscritura.write(primos.get(i));
+				bufferEscritura.newLine();
+			}
+
+			bufferEscritura.close();
+			System.out.println("Archivo creado correctamente");
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ AccesoADatos/src/tema1/ejercicios/ejerciciosIO/_01_02/lectura/ejercicio3.java
@@ -0,0 +1,71 @@
+package tema1.ejercicios.ejerciciosIO._01_02.lectura;
+
+import java.io.*;
+
+public class ejercicio3 {
+
+	public static void main(String[] args) {
+
+		File archivo = new File("Restaurants.csv");
+
+		if (!archivo.exists()) {
+			System.out.println("El archivo no existe");
+
+		} else {
+
+			try {
+				FileReader lector = new FileReader(archivo);
+				BufferedReader buffer = new BufferedReader(lector);
+
+				String linea = buffer.readLine();
+				System.out.println(linea);
+				String[] encabezado = linea.split(",");
+				while ((linea = buffer.readLine()) != null) {
+
+					boolean comillas = false;
+					StringBuilder sb = new StringBuilder(linea); // lo uso pq un string normal no puedo setearle el
+																	// valor de i, con el sb si puedo
+
+					if (linea.contains("\"")) {
+						for (int i = 0; i < sb.length(); i++) {
+
+							if (sb.charAt(i) == '"') {
+								comillas = !comillas;
+							}
+
+							if (comillas && sb.charAt(i) == ',') {
+								sb.setCharAt(i, '*');
+							}
+
+						}
+
+						String[] fraseComillas = sb.toString().split(",");
+
+						for (int i = 0; i < fraseComillas.length; i++) {
+							System.out.print("#" + encabezado[i] + ":" + fraseComillas[i].replace("*", ",") + "  ");
+						}
+						System.out.println();
+
+					} else {
+						String[] comas = linea.split(",");
+
+						for (int i = 0; i < comas.length; i++) {
+
+							System.out.print("#" + encabezado[i] + ":" + comas[i] + "  ");
+						}
+						System.out.println();
+					}
+
+				}
+
+				buffer.close();
+
+			} catch (IOException e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+}
